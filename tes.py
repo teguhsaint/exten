a98b18de0aabf266d7feb4d3f501193d50228600
Teguh Saint <75825300+teguhsaint@users.noreply.github.com>
2024-02-01 06:51:37 +0700

    s.

--- tes.py
+++ tes.py
@@ -1,1 +1,10 @@
-import os
+from BingImageCreator import ImageGen
+
+auth_cookie = "16uU6nOiYSCw58gLDaNGDCozUB8ywUP3TBicXHrV5YwCfXFwDC_KJQFHsV23uaodDKZ8d5T7zfO2QS-i4jWKQjnLSZA0YolgYHf8knXi8k2TZeyCBhsNRV3_cmUVgvoubpsH7Gqi3P_ftb2teQSrP4Xgj7e3cqTcqlxGePxxRnajUClPGV2gcrMfc5weUcF6XOUtgjSOSeOkFnhF-SkxJ5A"
+img_gen = ImageGen(auth_cookie,auth_cookie)
+
+prompt = input("Prompt : ")
+
+image_links = img_gen.get_images(f"\"{prompt}\"")
+output_dir = "kreasi"
+img_gen.save_images(image_links, output_dir)
